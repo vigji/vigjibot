b4d0eac4bdcef2fd741419910f6dd271c1db8057
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-04-26 11:13:59 +0200

    changed models

--- main.py
+++ main.py
@@ -363,14 +363,14 @@ if __name__ == "__main__":
         folder_to_save_reports_to=None,
         skip_previously_forecasted_questions=True,
         llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
-            "default": "openrouter/openai/gpt-4o",
+            "default": "openrouter/openai/o4-mini",
             # GeneralLlm(
             #     model="metaculus/anthropic/claude-3-5-sonnet-20241022",  # metaculus/anthropic
             #     temperature=0.3,
             #     timeout=40,
             #     allowed_tries=2,
             # ),
-            "summarizer": "openrouter/openai/o4-mini",
+            "summarizer": "openrouter/openai/gpt-4o-mini",
         },
     )
 
