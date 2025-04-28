33b2dfd401de779dc6f4e93951e70d28f05880d5
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-04-28 21:59:19 +0200

    made main configurable

--- main.py
+++ main.py
@@ -381,13 +381,13 @@ if __name__ == "__main__":
         folder_to_save_reports_to=None,
         skip_previously_forecasted_questions=True,
         llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
-            "default": "openrouter/anthropic/claude-3.7-sonnet",
-            # GeneralLlm(
-            #     model="metaculus/anthropic/claude-3-5-sonnet-20241022",  # metaculus/anthropic
-            #     temperature=0.3,
-            #     timeout=40,
-            #     allowed_tries=2,
-            # ),
+            # "default": "openrouter/anthropic/claude-3.7-sonnet",
+            "default": GeneralLlm(
+                model="openrouter/anthropic/claude-3.7-sonnet",  #"metaculus/anthropic/claude-3-5-sonnet-20241022",  # metaculus/anthropic
+                temperature=0.3,
+                timeout=40,
+                allowed_tries=2,
+            ),
             "summarizer": "openrouter/meta-llama/llama-4-maverick:free",
         },
     )
