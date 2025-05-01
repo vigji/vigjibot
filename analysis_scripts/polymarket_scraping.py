a78466ba2f9ba417649094c6090fd5617bda69a4
Luigi Petrucco <luigi.petrucco@gmail.com>
2025-05-02 00:54:42 +0200

    embedding of polym questions

--- analysis_scripts/polymarket_scraping.py
+++ analysis_scripts/polymarket_scraping.py
@@ -9,6 +9,8 @@ import requests
 from tqdm import tqdm
 from pprint import pprint
 import pandas as pd
+import numpy as np
+from openai import OpenAI
 
 # %%
 
@@ -58,23 +60,61 @@ for _ in tqdm(range(max_pages)):
 
 df = pd.DataFrame(all_markets)
 active_df = df[df["active"] & ~df["closed"]].reset_index(drop=True)
+active_df["num_tokens"] = active_df.apply(lambda x: len(x["tokens"]), axis=1)
+print(active_df["num_tokens"].value_counts())
+
+# %%
+active_df["question"].to_list()  #["question_text"] = active_df["question"].apply(lambda x: x["text"])
+# %%
+# %%
+model = "BAAI/bge-m3"
+
+# Create an OpenAI client with your deepinfra token and endpoint
+openai = OpenAI(
+    api_key=os.getenv("DEEPINFRA_TOKEN"),
+    base_url="https://api.deepinfra.com/v1/openai",
+)
+
+
+os.getenv("DEEPINFRA_TOKEN")
+questions_list = active_df["question"].to_list()
+sanitized_questions = [q.strip() for q in questions_list]
+
+# all_embeddings = []
+# for q in tqdm(sanitized_questions):
+#     if not q or len(q) < 2 or len(q) > 1000:
+#         print(q)
+#         all_embeddings.append(np.zeros(1024)*np.nan)
+#     else:
+#         embeddings = openai.embeddings.create(
+#             model=model, input=q, encoding_format="float"
+#         )
+#         all_embeddings.append(embeddings.data[0].embedding)
+cache_file = f"embeddings_polymarket_cache_{model.replace('/', '.')}.csv"
+chunk_size = 100
+if (Path(__file__).parent / cache_file).exists():
+    embeddings_df = pd.read_csv(cache_file, index_col=0)
+    embeddings_array = embeddings_df.to_numpy()
+    
+else:
+    embeddings_list = []
+    for i in tqdm(range(0, len(sanitized_questions), chunk_size)):
+        chunk = sanitized_questions[i:i+chunk_size]
+        embeddings = openai.embeddings.create(
+            model=model, input=chunk, encoding_format="float"
+        )
+        embeddings_list.extend(embeddings.data)
+
+    embeddings_array = np.array([embedding.embedding for embedding in embeddings_list])
+    embeddings_array.shape
+# %%
+embeddings_df = pd.DataFrame(embeddings_array, index=active_df["question_id"])
+embeddings_df.to_csv(cache_file)
+print(embeddings_array.shape)
 
 # %%
-sel_q = active_df.loc[0, :]
-sel_q
+meta_questions_df = pd.read_csv("questions_df.csv")
 # %%
-df["rewards"]
-# %%
-sel_q["tokens"]
-# %%
-q_id = 1746093315664
+meta_questions_df[0]
 
-# %%
-sel_q["question_id"]
-# %%
-df["question_id"]
-# %%# %%
-sel_q = active_df[active_df["market_slug"] == "us-recession-in-2025"]
-# %%
-sel_q.to_dict()
 # %%
